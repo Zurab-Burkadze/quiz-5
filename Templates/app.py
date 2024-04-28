7a7e0c2263bf5311ce1a115b1e6c2939ef8eef5d
Zurab-Burkadze <164529612+Zurab-Burkadze@users.noreply.github.com>
2024-04-28 12:59:57 +0400

    Add files via upload

---
+++ Templates/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template,redirect, url_for
+app = Flask(__name__)
+
+
+tasks =["lay down lavashi", "add shawarma meat", "add ketchup", "add onions", "add greens", "wrap it up and toast"]
+
+
+@app.route('/')
+def home():
+return render_template('index.html', tasks=tasks)
+
+
+@app.route('/add_task', methods=['GET', 'POST'])
+def add_task():
+if request.method == 'POST':
+task = request.form['task']
+tasks.append(task)
+return redirect(url_for('home'))
+return render_template('add_task.html' , tasks=tasks)
+
+
+if __name__ == '__main__':
+app.run(debug=True)
+
